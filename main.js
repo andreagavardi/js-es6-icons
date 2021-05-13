61c2ce6cf1e6f2fdde6b874509c045ef52fd7e63
andreagavardi <andreagavardi89@gmail.com>
2021-05-13 22:06:52 +0200

    esercizio terminato

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-Milestone 3 Creiamo una select con i tipi di icone e usiamola per filtrare le icone */
+
 
 
 const icons = [
@@ -104,8 +104,11 @@ const icons = [
 /* Milestone 1:
 Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout.
  */
+      
+
 
 icons.forEach(icon => {
+  
     switch (icon.type) {           // Milestone 2 Coloriamo le icone per tipo
         case 'user':
             icon.color='violet';        //aggiungo una proprietà agli oggetti per specificarne il colore
@@ -118,14 +121,52 @@ icons.forEach(icon => {
             break;   
     }
     
-    document.querySelector(".container").insertAdjacentHTML("beforeend",
-    `
-         <div class="card">
+    
+    document.querySelector(".container").insertAdjacentHTML("beforeend",`
+        <div class="card">
             <i class="${icon.family} ${icon.prefix}${icon.name} ${icon.color}"></i>
             <h3>${icon.name}</h3>
         </div>
-    `);
-  
-})
+    `);        
+    
+    
+    
+}); 
 
+
+let selectEl = document.getElementById("type");   // id del <select>
+
+selectEl.addEventListener("change", function(){             //ascolto per l'evento del change della selezione
+    document.querySelector(".container").innerHTML="";         //azzero ogni volta il mio markup
+    icons.forEach(icon => {
+        const markup = `
+            <div class="card">
+                    <i class="${icon.family} ${icon.prefix}${icon.name} ${icon.color}"></i>
+                    <h3>${icon.name}</h3>
+                </div>
+        `;
+        //per ogni valore del mio <select> imposto quando deve essere visibile
+        if(selectEl.value == "all"){
+           document.querySelector(".container").insertAdjacentHTML("beforeend",markup);        
+        }
+        else if (selectEl.value == "animal" && icon.type == "animal"){
+           document.querySelector(".container").insertAdjacentHTML("beforeend",markup);   
+
+        }else if(selectEl.value == "user" && icon.type == "user"){
+            document.querySelector(".container").insertAdjacentHTML("beforeend",markup);
+        }else if (selectEl.value == "vegetable" && icon.type == "vegetable"){
+            document.querySelector(".container").insertAdjacentHTML("beforeend",markup);
+        }
+    }) ;
+    
+});
+
+
+
+
+
+console.log(icons, );
 //Milestone 3 Creiamo una select con i tipi di icone e usiamola per filtrare le icone 
+
+
+
